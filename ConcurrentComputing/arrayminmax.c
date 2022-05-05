262a10cb904fdbd13a9ea4f46a9f8211d7ebd6e3
Matheus Mota dos Santos da Silva <88465285+MattMota@users.noreply.github.com>
2022-05-05 00:15:00 -0300

    Update arrayminmax.c
    FInal version

--- ConcurrentComputing/arrayminmax.c
+++ ConcurrentComputing/arrayminmax.c
@@ -1,4 +1,4 @@
-//Soma todos os elementos de um vetor de inteiro
+// Calcula o mínimo e o máximo de um vetor de float de muitas posições
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -14,12 +14,11 @@ typedef struct minmax {
 } minmax;
 
 //fluxo das threads
-void * tarefa(void * arg) {
+void * tarefa(void *arg) {
 
-    long long int id = (long long int) arg; //identificador da thread
-    minmax min_max; //variáveis locais do menor e maior elemento do bloco
-
-    minmax min_max = (minmax*) malloc(sizeof(minmax));
+    long long int id = (long long int) arg;
+    //variáveis locais do menor e maior elemento do bloco
+    minmax *min_max = (minmax*) malloc(sizeof(minmax));
     if (min_max == NULL) {
         exit(1);
     }
@@ -35,16 +34,16 @@ void * tarefa(void * arg) {
     } // Trata o resto se houver
 
     // Acha o menor e o maior elemento do bloco da thread
-    for (long int i = ini; i < fim; i++) {
+    for (long long int i = ini; i < fim; i++) {
         if (i == ini) {
             min_max->min = vetor[i];
             min_max->max = vetor[i];
         }
         else {
-            if (vetor[i] < min){
+            if (vetor[i] < min_max->min){
                 min_max->min = vetor[i];
             }
-            if (vetor[i] > max) {
+            if (vetor[i] > min_max->max) {
                 min_max->max = vetor[i];
             }
         }
@@ -56,16 +55,16 @@ void * tarefa(void * arg) {
 
 //fluxo principal
 int main(int argc, char *argv[]) {
-    float somaSeq = 0; //soma sequencial
-    float somaConc = 0; //soma concorrente
-    float ini, fim; //tomada de tempo
+    float ini, fim, t_seq, t_conc; //tomada de tempo
+    float min_conc; //minimo global
+    float max_conc; //maximo global
     pthread_t *tid; //identificadores das threads no sistema
-    float *retorno; //valor de retorno das threads
+    minmax *retorno_seq, *retorno_conc; //valores de retorno
 
     //recebe e valida os parametros de entrada (dimensao do vetor, numero de threads)
     if(argc < 3) {
         fprintf(stderr, "Digite: %s <dimensao do vetor> <numero threads>\n", argv[0]);
-        return 1; 
+        return 1;
     }
     dim = atoll(argv[1]);
     nthreads = atoi(argv[2]);
@@ -77,21 +76,46 @@ int main(int argc, char *argv[]) {
         return 2;
     }
 
+    //aloca a struct de retorno sequencial
+    retorno_seq = malloc(sizeof(minmax));
+    if (retorno_seq == NULL) {
+        fprintf(stderr, "ERRO--malloc\n");
+        return 2;
+    }
+
+    //aloca a struct de retorno concorrente
+    retorno_conc = malloc(sizeof(minmax));
+    if (retorno_conc == NULL) {
+        fprintf(stderr, "ERRO--malloc\n");
+        return 2;
+    }
+
     //preenche o vetor de entrada
     srand(time(NULL));
     for (long long int i = 0; i < dim; i++) {
         vetor[i] = rand();
     }
 
-    //soma sequencial dos elementos
+    //min_max sequencial dos elementos
     GET_TIME(ini);
     for(long long int i=0; i<dim; i++) {
-        somaSeq += vetor[i];
+        if (i == 0) {
+            retorno_seq->min = vetor[i];
+            retorno_seq->max = vetor[i];
+        }
+        else {
+            if (vetor[i] < retorno_seq->min){
+                retorno_seq->min = vetor[i];
+            }
+            if (vetor[i] > retorno_seq->max) {
+                retorno_seq->max = vetor[i];
+            }
+        }
     }
     GET_TIME(fim);
-    printf("Tempo sequencial:  %lf\n", fim - ini);
+    t_seq = fim - ini;
 
-    //soma concorrente dos elementos
+    //criando o vetor de threads
     GET_TIME(ini);
     tid = (pthread_t *) malloc(sizeof(pthread_t) * nthreads);
     if (tid == NULL) {
@@ -107,40 +131,46 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    long long int min;
-    long long int max;
+    
     //aguardar o termino das threads
     for(long long int i = 0; i<nthreads; i++) {
-        if(pthread_join(*(tid+i), (void**) &retorno)){
+        if(pthread_join(*(tid+i), (void**) &retorno_conc)){
             fprintf(stderr, "ERRO--pthread_create\n");
             return 3;
         }
+
         //análise de min-max global
         if (i == 0) {
-            min = retorno->min;
-            max = retorno->max;
+            min_conc = retorno_conc->min;
+            max_conc = retorno_conc->max;
         }
         else {
-            if (vetor[i] < min){
-                min = retorno->min;
+            if (retorno_conc->min < min_conc){
+                min_conc = retorno_conc->min;
             }
-            if (vetor[i] > max) {
-                max = retorno->max;
+            if (retorno_conc->max > max_conc) {
+                max_conc = retorno_conc->max;
             }
         }
-        free(retorno);
+        free(retorno_conc);
     }
     GET_TIME(fim);
-    printf("Tempo concorrente:  %lf\n", fim-ini);
+    t_conc = fim - ini;
 
     //exibir os resultados
-    printf("Minimo:  %.12lf\n", min);
-    printf("Maximo: %.12lf\n", max);
+    if ((min_conc == retorno_seq->min) && (max_conc == retorno_seq->max)) {
+        printf("Valores iguais!\nMinimo:  %.12lf\nMaximo: %.12lf\n\n", min_conc, max_conc);
+    }
+    else {
+        fprintf(stderr, "Valores diferentes!\n");
+        return 4; 
+    }
+    printf("%lf/%lf = %lf\n", t_seq, t_conc, t_seq/t_conc);
 
     //libera as areas de memoria alocadas
     free(vetor);
     free(tid);
-    min
+    free(retorno_seq);
 
     return 0;
 }
