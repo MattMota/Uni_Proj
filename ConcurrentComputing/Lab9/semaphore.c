4e9b7cef39b090790dca17d48cde01275c9a2c97
MattMota <matheusmota18rj@gmail.com>
2022-06-28 22:59:00 -0300

    Corrected semaphore.c in Lab9

--- ConcurrentComputing/Lab9/semaphore.c
+++ ConcurrentComputing/Lab9/semaphore.c
@@ -11,7 +11,7 @@ sem_t cond_meio, cond_fim;     //semaforos para sincronizar a ordem de execucao 
 
 // Função executada pelas threads
 void* threadFunc(void* tid) {
-    int* thread_id = (int*) tid;
+    long int thread_id = (long int) tid;
     switch (thread_id) {
         // Função executada pela thread 1
         case 1:
@@ -21,36 +21,43 @@ void* threadFunc(void* tid) {
             break;
         // Função executada pela thread 2
         case 2:
-            sem_wait(&cond_fim); // Espera T5 executar
+            sem_wait(&cond_meio); // Espera T5 executar
             printf("Fique a vontade.\n");
+            mensagens++; // Incrementa o contador de mensagens
             sem_post(&cond_meio); // Permite que T3 e T4 executem
-            sem_post(&cond_fim); // Permite que T1 execute se houve 3 posts desse
+            if (mensagens == 4) // Se todas as demais mensagens foram impressas
+                sem_post(&cond_fim); // Permite que T1 execute se houve 3 posts desse
             pthread_exit(NULL);
             break;
         // Função executada pela thread 3
         case 3:
             sem_wait(&cond_meio); // Espera T5 executar
             printf("Sente-se por favor.\n");
+            mensagens++; // Incrementa o contador de mensagens
             sem_post(&cond_meio); // Permite que T2 e T4 executem
-            sem_post(&cond_fim); // Permite que T1 execute se houve 3 posts desse
+            if (mensagens == 4) // Se todas as demais mensagens foram impressas
+                sem_post(&cond_fim); // Permite que T1 execute se houve 3 posts desse
             pthread_exit(NULL);
             break;
         // Função executada pela thread 4
         case 4:
             sem_wait(&cond_meio); // Espera T1 executar
             printf("“Aceita um copo d'agua?”\n");
+            mensagens++; // Incrementa o contador de mensagens
             sem_post(&cond_meio); // Permite que T2 e T3 executem
-            sem_post(&cond_fim); // Permite que T1 execute se houve 3 posts desse
+            if (mensagens == 4) // Se todas as demais mensagens foram impressas
+                sem_post(&cond_fim); // Permite que T1 execute se houve 3 posts desse
             pthread_exit(NULL);
             break;
         // Função executada pela thread 5
         case 5:
             printf("Seja bem-vindo!\n");
+            mensagens++; // Incrementa o contador de mensagens
             sem_post(&cond_meio); // Permite que T2, T3 e T4 executem
             pthread_exit(NULL);
             break;
         default:
-            return -1;
+            exit(-1); 
     }
 }
 
@@ -73,18 +80,18 @@ int main(int argc, char *argv[]) {
     sem_init(&cond_fim, 0, -2);
 
     // Criamos as threads individuais
-    for (int i = 0; i < NTHREADS; i++) {
-        if(pthread_create(&tid[i], NULL, threadFunc, (void*) i+1)){
+    for (long int i = 0; i < NTHREADS; i++) {
+        if(pthread_create(tid + i, NULL, threadFunc, (void*) i+1)){
             fprintf(stderr, "ERRO: pthread_create\n");
-            return 1;
+            exit(-1);
         } 
     }
 
     // Espera todas as threads terminarem
-    for (t=0; t<NTHREADS; t++) {
-        if (pthread_join(tid[t], NULL)) {
-            printf("ERRO: pthread_join() \n"); exit(-1); 
+    for (int i = 0; i<NTHREADS; i++) {
+        if (pthread_join(*(tid + i), NULL)) {
+            printf("ERRO: pthread_join() \n");
+            exit(-1); 
         } 
-        free(id[t]);
     }
 }
